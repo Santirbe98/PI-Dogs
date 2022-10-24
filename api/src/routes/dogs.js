951d00a97aee327170099e67425ef0e292e26b96
Santirbe98 <santirbe@gmail.com>
2022-10-24 20:22:53 -0300

    Rutas GET por id terminada, queda resolver lo de la base de datos POST

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,18 +1,46 @@
 const { Router } = require("express");
 const { getAllDogs } = require("./functions");
+const { Dog } = require("../db");
 const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
+    // Llama a la funcion GetAllDogs que trae todos los perros
     const allDogs = await getAllDogs();
     res.status(200).json(allDogs);
   } catch (error) {
-    res.status(500).send(`Internal server Error`);
+    next();
   }
 });
 
-router.get("/:id", async (req, res, next) => {});
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const idDog = await getAllDogs(); // trae todos los perros
+    // Primero busca el perro en la API
+    const filterDogApi = await idDog.Api_Dogs.find(
+      (dog) => dog.id === Number(id)
+    );
+    // Luego busca el perro en la base de datos
+    const filterDogDB = await idDog.DB_Dogs.find(
+      (dog) => dog.id === Number(id)
+    );
+    // Dependiendo de donde encuentre el perro manda el correspondiente en formato json
+    if (filterDogApi) {
+      res.status(200).json(filterDogApi);
+    }
+    if (filterDogDB) {
+      res.status(200).json(filterDogDB);
+    }
+    // Si no encuentra el perro envia un 404 con un texto diciendo que el perro no existe
+    res.status(404).send(`Dog doesn't exist`);
+  } catch (error) {
+    next();
+  }
+});
 
-router.post("/", async (req, res, next) => {});
+router.post("/", async (req, res, next) => {
+  // const { id, name, height, weight, life_span, breed_group, image } = req.body;
+});
 
 module.exports = router;
--- api/src/routes/temperament.js
+++ api/src/routes/temperament.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 const { Temperament } = require("../db");
 const router = Router();
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res, next) => {});
 
 module.exports = router;
